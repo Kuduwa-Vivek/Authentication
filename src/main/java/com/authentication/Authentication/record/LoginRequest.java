c8fdffe2c3f96b600d384254beb9e78be10ce272
Kuduwa-Vivek <kuduwav@gmail.com>
2024-12-14 01:05:42 +0530

    EN-0001 | Added loggin

--- src/main/java/com/authentication/Authentication/record/LoginRequest.java
+++ src/main/java/com/authentication/Authentication/record/LoginRequest.java
@@ -1,3 +1,14 @@
 package com.authentication.Authentication.record;
 
-public record LoginRequest(String username, String password) {}
+public record LoginRequest(String username, String password) {
+
+  @Override
+  public String username() {
+    return username;
+  }
+
+  @Override
+  public String password() {
+    return password;
+  }
+}
